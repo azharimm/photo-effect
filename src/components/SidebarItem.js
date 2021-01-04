301731171de79c0e5e24352b94ff9ccdb74a98fe
Azhari Marzan <azhari.marzan@gmail.com>
2021-01-04 14:58:23 +0700

    + sidebar item

---
+++ src/components/SidebarItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SidebarItem = (props) => {
+
+    return (
+        <button className="sidebar-item">SidebarItem</button>
+    )
+}
+
+export default SidebarItem;
