a02218d95cb3c356e53ec254a7bb780799697a2e
Azhari Marzan <azhari.marzan@gmail.com>
2021-01-04 14:50:25 +0700

    + slider

---
+++ src/components/Slider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Slider = (props) => {
+
+    return (
+        <div className="slider-container">
+            <input type="range" className="slider"/>
+        </div>
+    )
+}
+
+export default Slider;
